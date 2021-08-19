69a0ac5a58f21d589f46411fe243be0b88c4fbfd
samsterpiece <samantha.badeau@gmail.com>
2021-08-19 18:39:14 -0400

    Beginner Python Files--PluralSight Course

---
+++ Python/decades.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you? "))
+decadeCalculator = age // 10
+years = age % 10
+print("You are ", decadeCalculator, " decades and", years, " years old.")
---
+++ Python/hello.py
@@ -0,0 +1,6 @@
+hello = "Hello"
+name = input("What's your name?")
+greeting = hello + " " + name 
+print(greeting)
+
+
