d79e9e80e48822a096f8b65b4d1cecd6dc3efb55
samsterpiece <samantha.badeau@gmail.com>
2021-08-23 14:12:11 -0400

    small expenses calculator

---
+++ Python/expenses.py
@@ -0,0 +1,9 @@
+expenses = [10.50,8,5,15,20,5,3]
+total = sum(expenses)
+
+# #add the total of list
+# for x in expenses:
+#     sum = sum + x
+
+
+print("You spent $" , total, " on lunch this week.", sep = '')
