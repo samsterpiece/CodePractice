ef5cd4edce329626decad810c2d77158bda4ef3d
samsterpiece <samantha.badeau@gmail.com>
2021-08-31 11:30:31 -0400

    using dict to represent obj

---
+++ Python/person.py
@@ -0,0 +1,10 @@
+#Using dictionaries to represent objects
+
+person = {
+    "name": "Jane Smith",
+    "city" :  "Orlando",
+    "age" : "100"
+
+}
+
+print(person.get("name") , "is from " + person.get("city") , "and is" , person.get("age") , "years old.")
